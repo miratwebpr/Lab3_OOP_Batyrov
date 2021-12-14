a291d59ef1802dabd0fdbce4998d2cf2ad3bd789
spapua228М <spapua228@mail.ru>
2021-12-14 19:59:10 +0500

    Iterator fixed

--- Lab3_OOP_Batyrov/Lab3_OOP_Batyrov.cpp
+++ Lab3_OOP_Batyrov/Lab3_OOP_Batyrov.cpp
@@ -1,8 +1,10 @@
 ﻿
 
 #include "stdio.h"
+#include <stdlib.h>
 #include <conio.h>
-//Первым делом в начало
+#include <time.h>
+#define _CRT_SECURE_NO_WARNINGS
 template<class T>class Storage;
 
 template<class T>
@@ -117,30 +119,47 @@ public:
 	class Iterator
 	{
 	private:
-		Node<T>* first;
+		Node<T>* current;
 	public:
-		Iterator(Node<T>* first)
+		Iterator(Node<T>* current)
 		{
-			this->first = first;
+			this->current = current;
 		}
 		Iterator operator++(int)
 		{
-			Iterator itr = *this;
-			first = first->next;
+			Iterator itr = current;
+			current = current->next;
+			return itr;
+		}
+		Iterator operator--(int)
+		{
+			Iterator itr = current;
+			current = current->prev;
 			return itr;
 		}
 		T& operator*()
 		{
-			return first->Obj;
+			return current->Obj;
 		}
 		bool operator==(Iterator const comp) const
 		{
-			return first == comp.first;
+			return current == comp.current;
+		}
+		bool hasCurrent() const
+		{
+			if (current != NULL)
+				return true;
+			return false;
 		}
 		bool operator!=(Iterator const comp) const
 		{
-			return !(first == comp.first);
+			if (hasCurrent() == true && (current->next == NULL || current->prev == NULL))
+				return true;
+			else if (hasCurrent() == false)
+				return false;
+			return !(current == comp.current);
 		}
+
 	};
 	Iterator begin()
 	{
@@ -148,7 +167,7 @@ public:
 	}
 	Iterator end()
 	{
-		return Iterator(last->next);
+		return Iterator(last);
 	}
 };
 
@@ -164,7 +183,7 @@ public:
 	}
 	Point(int x, int y)
 	{
-		printf("Point(int x, y)\n");
+		//printf("Point(int x, y)\n");
 		this->x = x;
 		this->y = y;
 	}
@@ -220,44 +239,104 @@ public:
 using namespace std;
 int main()
 {
+	srand(time(NULL));
 	Storage <Point*> MyStorage;
-	Point *p1 = new Point(3, 19);
-	Point* p2 = new Point(1, 32);
-	Point* p3 = new Point(2, 125);
-	MyStorage.pushFront(p1);
-	MyStorage.pushBack(p2);
-	MyStorage.pushBack(p3);
-
-
-	Storage<Point*>::Iterator i = MyStorage.begin();
-	for (i; i != MyStorage.end(); i++)
+	int actAmount = 0;
+	int Action = 0;
+	// 0 - getCount
+	// 1 - create and pushBack
+	// 2 - create and pushFront
+	// 3 - getObject (byIndex)
+	// 4 - getObjectAndDel (byIndex)
+	// 5 - print all elements of the storage with iterator
+	printf("Choose amount of random actions that you want to be done with the storage\n");
+	scanf("%d", &actAmount);
+	for (int i = 0; i < actAmount; i++)
 	{
-		try
+		Action = rand() % 6 + 0;
+		printf("Action = %d\n", Action);
+		switch (Action)
 		{
-			Point* p4 = *i;
-			printf("%d\n", p4->x);
-		}
-		catch (int t)
-		{
-			if (t == 0) printf("Error: index is bigger than size of the storage");
+			case 0:
+			{
+				int count = MyStorage.getCount();
+				printf("Size of storage -- %d\n", count);
+				break;
+			}
+			case 1:
+			{
+				int x = rand() % 100 + 10;
+				int y = rand() % 100 + 10;
+				Point* p1 = new Point(x, y);
+				MyStorage.pushBack(p1);
+				printf("Object p1->x = %d successfully pushed in back of the storage\n", p1->x);
+				break;
+			}
+			case 2:
+			{
+				int x = rand() % 100 + 10;
+				int y = rand() % 100 + 10;
+				Point* p1 = new Point(x, y);
+				MyStorage.pushFront(p1);
+				printf("Object p1->x = %d successfully pushed in front of the storage\n", p1->x);
+				break;
+			}
+			case 3:
+			{
+				if (MyStorage.getCount() > 0)
+				{
+					int index = rand() % MyStorage.getCount() + 0;
+					try
+					{
+						Point* p4 = MyStorage.getObject(index);
+						printf("MyStorage.getObject(%d) = p4->x = %d\n", index, p4->x);
+					}
+					catch (int t)
+					{
+						if (t == 0) printf("Error: index is bigger than size of the storage or equal");
+					}
+				}
+				break;
+			}
+
+			case 4: 
+			{
+				if (MyStorage.getCount() > 0)
+				{
+					int index = rand() % MyStorage.getCount() + 0;
+					try
+					{
+						Point* p4 = MyStorage.getObjectAndDel(index);
+						printf("MyStorage.getObjectAndDel(%d) = p4->x = %d\n", index, p4->x);
+					}
+					catch (int t)
+					{
+						if (t == 0) printf("Error: index is bigger than size of the storage or equal");
+					}
+				}
+				break;
+			}		
+			case 5:
+			{
+				Storage<Point*>::Iterator iter = MyStorage.end();
+				int j = 0;
+				for (; iter.hasCurrent(); iter--)
+				{
+					
+					try
+					{
+						Point* p4 = *iter;
+						printf("Getting all elements of Storage: j = %d, *iter->x = %d\n\n", j, p4->x);
+						j++;
+					}
+					catch (int t)
+					{
+						if (t == 0) printf("Error: index is bigger than size of the storage or equal");
+					}
+				}
+				break;
+			}
+			
 		}
 	}
 }
-/*
-Point* p5 = new Point(1, 32);
-Point* p6 = new Point(2, 125);
-MyStorage.pushFront(p5);
-MyStorage.pushBack(p6);
-for (int i = 0; i < MyStorage.getCount(); i++)
-{
-	try
-	{
-		Point* p4 = MyStorage.getObject(i);
-		printf("%d\n", p4->x);
-	}
-	catch (int t)
-	{
-		if (t == 0) printf("Error: index is bigger than size of the storage");
-	}
-}
-*/
